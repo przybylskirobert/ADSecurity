816270c85e7ccff3300c1d4a1e78a3560126ef9e
przybylskirobert <56112893+przybylskirobert@users.noreply.github.com>
2020-04-13 18:59:58 +0200

    Update Create-Group.ps1
    update with the example usage

--- Tiering/Create-Group.ps1
+++ Tiering/Create-Group.ps1
@@ -1,3 +1,18 @@
+<#
+    .Example
+    $csv = Read-Host -Prompt "Please provide full path to Groups csv file"
+    .\Create-Group.ps1 -CSVfile $csv -Verbose
+    PS C:\Tools> $csv = Read-Host -Prompt "Please provide full path to Groups csv file"
+    Please provide full path to Groups csv file: c:\tools\groups.csv
+    PS C:\Tools> .\Create-Group.ps1 -CSVfile $csv -Verbose
+    VERBOSE: Creating new Group 'Tier0ReplicationMaintenance' under 'OU=Groups,OU=Tier0,OU=Admin,DC=azureblog,DC=pl'
+    VERBOSE: Creating new Group 'Tier1ServerMaintenance' under 'OU=Groups,OU=Tier1,OU=Admin,DC=azureblog,DC=pl'
+    VERBOSE: Creating new Group 'ServiceDeskOperators' under 'OU=Groups,OU=Tier2,OU=Admin,DC=azureblog,DC=pl'
+    VERBOSE: Creating new Group 'WorkstationMaintenance' under 'OU=Groups,OU=Tier2,OU=Admin,DC=azureblog,DC=pl'
+    VERBOSE: Group 'tier1admins'already exists.
+    VERBOSE: Group 'tier2admins'already exists.
+#>
+
 [CmdletBinding()]
 param(
     [string] $CSVfile
