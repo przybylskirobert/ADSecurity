e1a7969844dce0edce166ff41908385823e98289
przybylskirobert <56112893+przybylskirobert@users.noreply.github.com>
2020-04-13 17:29:41 +0200

    Update Create-OU.ps1
    Added Examples

--- Tiering/Create-OU.ps1
+++ Tiering/Create-OU.ps1
@@ -1,3 +1,34 @@
+<#
+    .Example
+    Atempt to create OU that not exists in the desired path
+    $OUs = @(
+    $(New-Object PSObject -Property @{Name = "Desktops"; ParentOU = "ou=Workstations" }),
+    $(New-Object PSObject -Property @{Name = "Kiosks"; ParentOU = "ou=Workstations" }),
+    $(New-Object PSObject -Property @{Name = "Laptops"; ParentOU = "ou=Workstations" }),
+    $(New-Object PSObject -Property @{Name = "Staging"; ParentOU = "ou=Workstations" })
+    )
+    .\Create-OU.ps1 -OUs $OUs -Verbose
+    PS C:\Tools> .\Create-OU.ps1 -OUs $OUs -Verbose
+    VERBOSE: Creating new OU 'OU=Desktops,ou=Workstations,DC=azureblog,DC=pl'
+    VERBOSE: Creating new OU 'OU=Kiosks,ou=Workstations,DC=azureblog,DC=pl'
+    VERBOSE: Creating new OU 'OU=Laptops,ou=Workstations,DC=azureblog,DC=pl'
+    VERBOSE: Creating new OU 'OU=Staging,ou=Workstations,DC=azureblog,DC=pl'
+    .Example
+    Atempt to create OU that already exists in the desired path
+    $OUs = @(
+    $(New-Object PSObject -Property @{Name = "Desktops"; ParentOU = "ou=Workstations" }),
+    $(New-Object PSObject -Property @{Name = "Kiosks"; ParentOU = "ou=Workstations" }),
+    $(New-Object PSObject -Property @{Name = "Laptops"; ParentOU = "ou=Workstations" }),
+    $(New-Object PSObject -Property @{Name = "Staging"; ParentOU = "ou=Workstations" })
+    )
+    .\Create-OU.ps1 -OUs $OUs -Verbose
+    PS C:\Tools> .\Create-OU.ps1 -OUs $OUs -Verbose
+    VERBOSE: OU 'Desktops' already exists under 'ou=Workstations,DC=azureblog,DC=pl'
+    VERBOSE: OU 'Kiosks' already exists under 'ou=Workstations,DC=azureblog,DC=pl'
+    VERBOSE: OU 'Laptops' already exists under 'ou=Workstations,DC=azureblog,DC=pl'
+    VERBOSE: OU 'Staging' already exists under 'ou=Workstations,DC=azureblog,DC=pl
+#>
+
 [CmdletBinding()]
 param(
     [PSObject] $OUs
