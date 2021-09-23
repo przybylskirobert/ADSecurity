e0b0074556d82aa1a5c58d7d074222db3819e011
Robert Przybylski <56112893+przybylskirobert@users.noreply.github.com>
2021-09-23 23:31:37 +0200

    script update

--- PAW/Create-User.ps1
+++ PAW/Create-User.ps1
@@ -19,10 +19,11 @@ foreach ($user in $users) {
     $UserPrincipalName = $samAccountName + '@' + $DNSRoot
     $parentOU = $user.ParentOU + ',' + $DSN
     $groupMembership = $user.GroupMembership
+    $enabled = [bool]$user.enabled
     $checkForUser = [bool]( Get-ADUSer -Filter {SamAccountname -eq $samaccountname})
     If ($checkForUser -eq $false) {
         Write-Verbose "Creating new user '$samAccountName' under '$parentOU'"
-        New-ADUser -Name $name -Path $ParentOU -SamAccountName $samAccountName -UserPrincipalName $UserPrincipalName -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -ChangePasswordAtLogon $false -Enabled $true -EmailAddress $UserPrincipalName
+        New-ADUser -Name $name -Path $ParentOU -SamAccountName $samAccountName -UserPrincipalName $UserPrincipalName -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -ChangePasswordAtLogon $false -Enabled $enabled -EmailAddress $UserPrincipalName
         start-sleep -Seconds 5
         if ($groupMembership -ne "") {
             $groupMembership = ($user.GroupMembership) -split ','
@@ -34,6 +35,6 @@ foreach ($user in $users) {
         $error.Clear()
     } 
     Else {
-        Write-Verbose "User '$samAccountName'already exists."
+        Write-Verbose "User '$samAccountName' already exists."
     }
 }
