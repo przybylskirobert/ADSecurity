f239d5a8edc6fdc7e9d17c6f06252be94ede6e0e
Robert Przybylski <56112893+przybylskirobert@users.noreply.github.com>
2023-02-11 16:51:27 +0100

    Update LAPS_steps.ps1

--- LAPS/LAPS_steps.ps1
+++ LAPS/LAPS_steps.ps1
@@ -3,13 +3,15 @@ $location = Get-Location
 Set-Location C:\Tools\LAPS
 $dsnAME = (Get-ADDomain).DistinguishedName
 $domain = $env:USERDNSDOMAIN
+
 Throw "Please download LAPS from aka.ms/laps and put the msi files into the C:\Tools\LAPS\LAPS"
+
 #Copy LAPS msi files to sysvol
-    .\CopyTo-Sysvol.ps1 -FilesPath C:\tools\LAPS\LAPS -CustomSysvolPlacement -Verbose
+    .\CopyTo-Sysvol.ps1 -FilesPath C:\ADSecurity\LAPS\LAPS -Verbose
 
 #schema extension with LAPS     #64 on DC LAB
-        $lapsPath = "\\$Domain\SysVol\$Domain\Scripts\LAPs\LAPS.x64.msi"
-        $expression = "C:\Windows\System32\msiexec.exe /i $LapsPath ADDLOCAL=CSE,Management,Management.UI,Management.PS,Management.ADMX /quiet"
+        $lapsPath = "\\$Domain\SysVol\$Domain\Scripts\LAPS\LAPS.x64.msi"
+        $expression = "C:\Windows\System32\msiexec.exe /i $lapsPath ADDLOCAL=CSE,Management,Management.UI,Management.PS,Management.ADMX /quiet"
         Invoke-Expression $expression
 <#
     #64 on PAW
@@ -27,39 +29,39 @@ Throw "Please download LAPS from aka.ms/laps and put the msi files into the C:\T
 
 #Allow computers  to store passwords
     Import-module AdmPwd.PS  
-    Set-AdmPwdComputerSelfPermission -Identity "OU=PAW,OU=Tier0,OU=Admin,$dsname"
-    Set-AdmPwdComputerSelfPermission -Identity "OU=Servers,OU=Tier0,OU=Admin,$dsname"
-    Set-AdmPwdComputerSelfPermission -Identity "OU=PAW,OU=Tier1,OU=Admin,$dsname"
-    Set-AdmPwdComputerSelfPermission -Identity "OU=Servers,OU=Tier1,OU=Admin,$dsname"
-    Set-AdmPwdComputerSelfPermission -Identity "OU=Computers,OU=AzureBlog,$dsname"
+    Set-AdmPwdComputerSelfPermission -Identity "OU=Devices,OU=Tier0,OU=Admin,$dsname"
+    Set-AdmPwdComputerSelfPermission -Identity "OU=Tier0 Servers,OU=Tier0,OU=Admin,$dsname"
+    Set-AdmPwdComputerSelfPermission -Identity "OU=Devices,OU=Tier1,OU=Admin,$dsname"
+    Set-AdmPwdComputerSelfPermission -Identity "OU=Tier 1 Servers,$dsname"
+    Set-AdmPwdComputerSelfPermission -Identity "OU=Workstations,$dsname"
     Set-AdmPwdComputerSelfPermission -Identity "OU=Quarantine,$dsname"
 
 #Allow users to read passwords
     Import-module AdmPwd.PS  
-    Set-AdmPwdReadPasswordPermission -Identity "OU=PAW,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "t0-admins"
-    Set-AdmPwdReadPasswordPermission -Identity "OU=Servers,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "t0-admins"
-    Set-AdmPwdReadPasswordPermission -Identity "OU=PAW,OU=Tier1,OU=Admin,$dsname" -AllowedPrincipals "t0-admins","t1-admins"
-    Set-AdmPwdReadPasswordPermission -Identity "OU=Servers,OU=Tier1,OU=Admin,$dsname" -AllowedPrincipals "t0-admins","t1-admins"
-    Set-AdmPwdReadPasswordPermission -Identity "OU=Computers,OU=AzureBlog,$dsname" -AllowedPrincipals "t0-admins","t1-admins"
-    Set-AdmPwdReadPasswordPermission -Identity "OU=Quarantine,$dsname" -AllowedPrincipals "t0-admins","t2-admins"
+    Set-AdmPwdReadPasswordPermission -Identity "OU=Devices,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "Domain Admins"
+    Set-AdmPwdReadPasswordPermission -Identity "OU=Tier0 Servers,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "Domain Admins"
+    Set-AdmPwdReadPasswordPermission -Identity "OU=Devices,OU=Tier1,OU=Admin,$dsname" -AllowedPrincipals "Domain Admins","tier1admins"
+    Set-AdmPwdReadPasswordPermission -Identity "OU=Tier 1 Servers,$dsname" -AllowedPrincipals "Domain Admins","tier1admins"
+    Set-AdmPwdReadPasswordPermission -Identity "OU=Workstations,$dsname" -AllowedPrincipals "Domain Admins","tier1admins"
+    Set-AdmPwdReadPasswordPermission -Identity "OU=Quarantine,$dsname" -AllowedPrincipals "Domain Admins","tier2admins"
 
 #Alow users to reset passwords
     Import-module AdmPwd.PS  
-    Set-AdmPwdResetPasswordPermission -Identity "OU=PAW,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "t0-admins"
-    Set-AdmPwdResetPasswordPermission -Identity "OU=Servers,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "t0-admins"
-    Set-AdmPwdResetPasswordPermission -Identity "OU=PAW,OU=Tier1,OU=Admin,$dsname" -AllowedPrincipals "t0-admins","t1-admins"
-    Set-AdmPwdResetPasswordPermission -Identity "OU=Servers,OU=Tier1,OU=Admin,$dsname" -AllowedPrincipals "t0-admins","t1-admins"
-    Set-AdmPwdResetPasswordPermission -Identity "OU=Computers,OU=AzureBlog,$dsname" -AllowedPrincipals "t0-admins","t1-admins"
-    Set-AdmPwdResetPasswordPermission -Identity "OU=Quarantine,$dsname" -AllowedPrincipals "t0-admins","t2-admins"
+    Set-AdmPwdResetPasswordPermission -Identity "OU=Devices,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "Domain Admins"
+    Set-AdmPwdResetPasswordPermission -Identity "OU=Tier0 Servers,OU=Tier0,OU=Admin,$dsname" -AllowedPrincipals "Domain Admins"
+    Set-AdmPwdResetPasswordPermission -Identity "OU=Devices,OU=Tier1,OU=Admin,$dsname" -AllowedPrincipals "Domain Admins","tier1admins"
+    Set-AdmPwdResetPasswordPermission -Identity "OU=Tier 1 Servers,$dsname" -AllowedPrincipals "Domain Admins","tier1admins"
+    Set-AdmPwdResetPasswordPermission -Identity "OU=Workstations,$dsname" -AllowedPrincipals "Domain Admins","tier1admins"
+    Set-AdmPwdResetPasswordPermission -Identity "OU=Quarantine,$dsname" -AllowedPrincipals "Domain Admins","tier2admins"
 
 #LAPS Installation GPO
 	Name: LAPSInstallation-v1.0
     Source Starter GPO: (none)
     GPO Status: User configuration settings disabled
     Category 	 	 	 	Package Placement	 	 	 	 	 	 	 	Deploy Software	Additional Info
-    Software Installation	\\$domain\sysvol\$domain\scripts\LAPS.x64.msi   Assigned	
+    Software Installation	\\$domain\sysvol\$domain\scripts\LAPS\LAPS.x64.msi   Assigned	
     Category 	 	 	 	Package Placement	 	 	 	 	 	 	 	Deploy Software	Additional Info
-    Software Installation	\\$domain\sysvol\$domainscripts\LAPS.x86.msi    Assigned		Uncheck Make this 32-bit x86 appliction available to Win64 machines
+    Software Installation	\\$domain\sysvol\$domainscripts\LAPS\LAPS.x86.msi    Assigned		Uncheck Make this 32-bit x86 appliction available to Win64 machines
 
 #LAPS Configuration Policy
     Name: LAPSConfiguration-v1.0
@@ -69,6 +71,3 @@ Throw "Please download LAPS from aka.ms/laps and put the msi files into the C:\T
 
     Administrative Templates 	LAPS			Password Settings						Enabled
                                                                                         Password Complexity: Large letters + small letters + numbers + specials
-                                                                                        Password Length: 14
-                                                                                        Password Age (Days): 30
-    Administrative Templates 	LAPS			Enable local admin password management	Enabled
